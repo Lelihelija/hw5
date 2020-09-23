9ca2ec79b115243396e20eb5c95fefa1f08a5b3c
Lelihelija <lelihelija@gmail.com>
2020-09-23 15:27:56 +0300

    Updates

---
+++ precache-manifest.27e248936103552cbcf33e4d2863e28f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d501e9249b37cd9c6a1eee8b6c4158",
+    "url": "/hw5/index.html"
+  },
+  {
+    "revision": "a852f7123835e434ec4c",
+    "url": "/hw5/static/css/main.1e5b13ad.chunk.css"
+  },
+  {
+    "revision": "81cc35cac56b16e8ff5e",
+    "url": "/hw5/static/js/2.985e9e5b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hw5/static/js/2.985e9e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a852f7123835e434ec4c",
+    "url": "/hw5/static/js/main.19819839.chunk.js"
+  },
+  {
+    "revision": "51a07fac054ccecd8f2e",
+    "url": "/hw5/static/js/runtime-main.c2e6d5cc.js"
+  },
+  {
+    "revision": "55d415620b585ca094baece2aabd1afa",
+    "url": "/hw5/static/media/1.55d41562.jpg"
+  },
+  {
+    "revision": "eeb20aa91e75c867aac54d482c15451e",
+    "url": "/hw5/static/media/10.eeb20aa9.jpg"
+  },
+  {
+    "revision": "b7a6ad10f90d767c18051ceb894476c6",
+    "url": "/hw5/static/media/2.b7a6ad10.jpg"
+  },
+  {
+    "revision": "731fa9fa67ec79cb8d4f207be34539be",
+    "url": "/hw5/static/media/3.731fa9fa.jpg"
+  },
+  {
+    "revision": "269a04c37d6e377201d53072230c81ce",
+    "url": "/hw5/static/media/4.269a04c3.jpg"
+  },
+  {
+    "revision": "8412b1a3563df13c681218fa77031b14",
+    "url": "/hw5/static/media/5.8412b1a3.jpg"
+  },
+  {
+    "revision": "f46abf40636ef0c8db08ce6d688c13d6",
+    "url": "/hw5/static/media/6.f46abf40.png"
+  },
+  {
+    "revision": "d048f8ab7c93e2f8e2ae6ca1d6136587",
+    "url": "/hw5/static/media/7.d048f8ab.jpg"
+  },
+  {
+    "revision": "57275a83d84792fa86424ec4e652a30d",
+    "url": "/hw5/static/media/8.57275a83.png"
+  },
+  {
+    "revision": "1de5c573eda05c0679016ea145f94ed0",
+    "url": "/hw5/static/media/9.1de5c573.jpg"
+  },
+  {
+    "revision": "2e0a702630c8987cc52de3fefb92ddc4",
+    "url": "/hw5/static/media/bokuto.2e0a7026.jpg"
+  },
+  {
+    "revision": "60e3e4afe1ff85400e2e892c0e635cd3",
+    "url": "/hw5/static/media/kageyamaLogo.60e3e4af.jpg"
+  },
+  {
+    "revision": "1f25847255acc4eac847b709f37ad74f",
+    "url": "/hw5/static/media/kageyamaPostImg2.1f258472.png"
+  },
+  {
+    "revision": "25c39c7972126fcda8690bd5e45eca22",
+    "url": "/hw5/static/media/kageyamapost.25c39c79.jpg"
+  },
+  {
+    "revision": "049528162d54b9e6e1bb964ddb254841",
+    "url": "/hw5/static/media/kurooLogo.04952816.png"
+  },
+  {
+    "revision": "ad964bf222a396d1ee52159c3a123966",
+    "url": "/hw5/static/media/owl.ad964bf2.jpg"
+  }
+]);
